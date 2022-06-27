a449339f5aa25b298a4397016249a4e71208b943
Shrey <shreybirmiwal@gmail.com>
2022-06-26 22:14:32 -0500

    Installing fonts

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -4,12 +4,12 @@ import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
 const RestaurantCard = styled(Card)`
-background-color: ${(props) => props.theme.colors.primary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
 padding: ${(props) => props.theme.space[3]}
-background-color: ${(props) => props.theme.colors.primary};
+background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const Title = styled(Text)`
