443bd0678931f67a6449a780e09e95c41338b3c7
Shrey <shreybirmiwal@gmail.com>
2022-06-26 22:27:18 -0500

    fonts and address on restaurant card component

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -12,9 +12,21 @@ padding: ${(props) => props.theme.space[3]}
 background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
-const Title = styled(Text)`
-  padding: 16px;
+const Address = styled(Text)`
+  font-family: ${(props)=> props.theme.fonts.body}
+  font-size: ${(props) => props.theme.fontSizes.caption}
   color: ${(props)=>props.theme.colors.ui.primary};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props)=> props.theme.fonts.heading}
+  font-size: ${(props) => props.theme.fontSizes.body}
+  color: ${(props)=>props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]}
+
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -33,7 +45,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+     <Info>
       <Title>{name}</Title>
+      <Address>{address}</Address>
+     </Info>
     </RestaurantCard>
   );
 };
